d9dada32be02dcb5a8c0c31967dcc40883cef3a2
heguopei <2386824052@qq.com>
2020-03-22 13:42:02 +0800

    该仓库的eslint是我见过的最傻叉的，最智障的代码提示工具，前一次提交没任何问题，第二次那个文件一点没改，居然不能commit，还抛错，智障成这样子，搞到我整整一个小时提交不了代码，又找不到错在哪里

--- src/layouts/BasicLayout.jsx
+++ src/layouts/BasicLayout.jsx
@@ -42,10 +42,7 @@ const noMatch = (
 // };
 
 const menuDataRender = menuList => {
-  menuList.map(item => {
-    const localItem = { ...item, children: item.children ? menuDataRender(item.children) : [] };
-    return localItem;
-  });
+  menuList.map(item => ({ ...item, children: item.children ? menuDataRender(item.children) : [] }));
   return menuList;
 };
 
